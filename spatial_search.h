7b580638a36a9dc7d60598463e0e5aa173165e60
hrbigelow <hbigelow@amgen.com>
2014-11-11 16:19:23 -0800

    spatial search using neighborhood density estimate

---
+++ spatial_search.h
@@ -0,0 +1,41 @@
+#ifndef _SPATIAL_SEARCH_H
+#define _SPATIAL_SEARCH_H
+
+#define NDIM 3
+
+struct marked_point {
+    double p[NDIM]; /* the underlying spatial coordinates of this point */
+    double dist_val; /* value of the underlying distribution at this point */
+    unsigned char in_grid[NDIM]; /* true if this point is in the grid
+                                    at that dimension */
+    struct marked_point *center; /* the center point for this search */
+    struct marked_point *prev; /* points to the previous marked_point
+                                  in the linked list of up to G
+                                  nearest neighbors */
+    double radius; /* the distance from this point to the originating
+                      point */
+};
+
+
+void set_cmp_dim(unsigned c);
+
+/* comparison by marked_point::p[cmp_dim].  cmp_dim is external */
+int marked_point_comp(const void *pa, const void *pb);
+
+
+/* estimate the volume associated with a particular center
+   point. 'head' is the head of a linked list of the G nearest
+   neighbors that terminates at the center point in question */
+double estimate_volume(struct marked_point *head, unsigned G);
+
+/* returns the head of a linked list of the G nearest neighbors of a
+   given point. it is the caller's responsibility to clear the in_grid
+   flags of all points.  return NULL if the search hit a boundary. */
+struct marked_point *
+spatial_search(struct marked_point **points[NDIM],
+               unsigned npoints,
+               struct marked_point *center,
+               int G);
+
+
+#endif /* _SPATIAL_SEARCH_H */
--- test_distance.c
+++ test_distance.c
@@ -2,7 +2,10 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_randist.h>
 #include <math.h>
+#include <time.h>
+#include <string.h>
 
+#include "spatial_search.h"
 
 /*
   In 1, 2, or 3 dimensions, simulate N points in the unit hypercube
@@ -13,152 +16,334 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 
-int dcomp(const void *a, const void *b)
+struct wpoint {
+    double *x;
+    double wgt;
+};
+
+int double_comp(const void *a, const void *b)
 {
     double av = *(double *)a, bv = *(double *)b;
     return av < bv ? -1 : ((av == bv) ? 0 : 1);
 }
 
+
 static unsigned cmp_dim = 0;
 
-/* qsort presents the comparison function with the addresses of two
-   elements.*/
+// compare wpoint *pa and *pb
 int pcmp(const void *pa, const void *pb)
 {
-    const double *a = *(const double **)pa, *b = *(const double **)pb;
+    const struct wpoint 
+        *a = (struct wpoint *)pa, 
+        *b = (struct wpoint *)pb;
+
     return 
-        a[cmp_dim] < b[cmp_dim] ? -1 : a[cmp_dim] > b[cmp_dim] ? 1 : 0;
+        a->x[cmp_dim] < b->x[cmp_dim] ? -1 : a->x[cmp_dim] > b->x[cmp_dim] ? 1 : 0;
 }
 
 
-/* returns the left-most pointer p in [base, base + nmemb * size)
-   where compar(p, val) >= 0 */
-void *lower_bound(void *base, size_t nmemb, size_t size, 
-                  void *val, /* address of the value to be fed into the comparison function */
-                  int(*compar)(const void *, const void *))
+int wcmp(const void *pa, const void *pb)
 {
-    size_t half;
-    void *first = base, *middle;
-    while (nmemb > 0)
-    {
-        half = nmemb >> 1;
-        middle = first;
-        middle += half * size;
-        if (compar(middle, val) < 0)
-        {
-            first = middle;
-            first += size;
-            nmemb = nmemb - half - 1;
-        }
-        else
-            nmemb = half;
-    }
-    return first;
+    const struct wpoint 
+        *a = (struct wpoint *)pa, 
+        *b = (struct wpoint *)pb;
+
+    return a->wgt < b->wgt ? -1 : a->wgt > b->wgt ? 1 : 0;
 }
 
+#define SQRT2 1.414213562373095048801688724209
+#define SQRT3 1.732050807568877293527446341505
 
-/* returns the right-most pointer p in [base, base + nmemb * size)
-   where compar(val, p) < 0 or p == base + nmemb * size if this is not
-   true of any element. */
-void *upper_bound(void *base, size_t nmemb, size_t size, 
-                  void *val, /* address of the value to be fed into the comparison function */
-                  int(*compar)(const void *, const void *))
+static double simplex2[] = {
+    -1.0 / 2.0,
+    1.0 / 2.0
+};
+
+static double simplex3[] = {
+    1.0 / 2.0, 0,
+    -1.0 / 2.0, 0,
+    0, SQRT3 / 2.0
+};
+
+static double simplex4[] = {
+
+    1,  0, -1.0 / SQRT2,
+    -1, 0, -1.0 / SQRT2,
+    0,  1,  1.0 / SQRT2,
+    0, -1,  1.0 / SQRT2
+};
+
+
+static double LO_BOUND = 0.1;
+static double HI_BOUND = 0.9;
+
+/* N normalized components fit into N-1 dimensions.  The simplex
+   corners are N points, each of N-1 dimensions.  The result is an N-1
+   dimensional 'point' */
+void comp_to_simplex(double *comp, double *point, int ncomp)
 {
-    size_t half;
-    void *first = base, *middle;
-    while (nmemb > 0)
+    int ci, di, ndim = ncomp - 1;
+    double *p;
+    switch (ncomp)
     {
-        half = nmemb >> 1;
-        middle = first;
-        middle += half * size;
-        if (compar(val, middle) < 0)
-            nmemb = half;
-        else
-        {
-            first = middle;
-            first += size;
-            nmemb = nmemb - half - 1;
-        }
+    case 2: p = simplex2; break;
+    case 3: p = simplex3; break;
+    case 4: p = simplex4; break;
     }
-    return first;
+
+    /* iterate over the N simplex corners.  For each one, add its
+       contribution to each of the N-1 dimensions */
+    for (di = 0; di != ndim; ++di)
+    {
+        point[di] = 0;
+        for (ci = 0; ci != ncomp; ++ci)
+            point[di] += p[ci * ndim + di] * comp[ci];
+    }
 }
 
 
-double euclidean_dist(double *p1, double *p2, size_t ndim)
+/*
+double euclidean_dist(double *p1, double *p2, int ncomp, int *ndim)
 {
     int d;
     double sq_dist = 0;
-    for (d = 0; d != ndim; ++d)
+    *ndim = ncomp;
+
+    for (d = 0; d != ncomp; ++d)
         sq_dist += gsl_pow_2(p1[d] - p2[d]);
 
     return sqrt(sq_dist);
 }
+*/
+
+static double *global_alpha;
+
+double dirichlet_pdf(double *point, int ndim)
+{
+    return gsl_ran_dirichlet_pdf(ndim, global_alpha, point);
+}
+
+
+/* points are a set of weighted points sampled from some
+   non-normalized distribution P(*), each weighted by the value of
+   P(*) at that point.  For each point p in points, estimate its
+   'neighborhood volume' v by finding its G nearest neighbors.
+   Calculate mass as v * P(p) for each point.  Finally, return median
+   and mean of masses filtered somehow... */
+void partite_mass(struct wpoint *points, unsigned npoints, unsigned G,
+                  double min_quantile, double max_quantile)
+{
+    /* build the marked_point arrays */
+    struct marked_point **mpoints[NDIM];
+
+    struct marked_point 
+        *mbuf = (struct marked_point *)malloc(npoints * sizeof(struct marked_point)),
+        *mcur,
+        *mend = mbuf + npoints;
+
+    /* initialize the content from wpoints to marked_points */
+    struct wpoint *wp = points;
+    mcur = mbuf;
+    while (mcur != mend)
+    {
+        memcpy(mcur->p, wp->x, sizeof(mcur->p));
+        mcur->dist_val = wp->wgt;
+        mcur->center = NULL; /* this is the only thing we need to mark this as 'uninitialized' */
+        ++mcur;
+        ++wp;
+    }
+
+    int p, d;
+
+    /* initialize all pointers in mpoints[] */
+    for (d = 0; d != NDIM; ++d)
+    {
+        mpoints[d] = (struct marked_point **)malloc(npoints * sizeof(struct marked_point *));
+        struct marked_point
+            **mpcur = mpoints[d],
+            **mpend = mpcur + npoints;
+        mcur = mbuf;
+        while (mpcur != mpend)
+            *mpcur++ = mcur++;
+    }
+
+    /* sort the points by individual dimension */
+    for (d = 0; d != NDIM; ++d)
+    {
+        set_cmp_dim(d);
+        qsort(mpoints[d], npoints, sizeof(struct marked_point *), marked_point_comp);
+    }    
+
+    /* for each point, do the spatial search, and update the metrics
+       of interest */
+    struct marked_point *head;
+    unsigned total_used = 0;
+    double *volume = (double *)malloc(npoints * sizeof(double *));
+    double *weight = (double *)malloc(npoints * sizeof(double *));
+    double *mass = (double *)malloc(npoints * sizeof(double *));
+
+    for (p = 0; p != npoints; ++p)
+    {
+        head = spatial_search(mpoints, npoints, mpoints[0][p], G);
+        /* estimate the neighborhood volume somehow */
+        if (! head)
+            continue;
+
+        volume[total_used] = estimate_volume(head, G);
+        weight[total_used] = head->center->dist_val;
+        mass[total_used] = volume[total_used] * weight[total_used];
+        ++total_used;
+    }
+
+    /* report raw metrics */
+    fprintf(stdout, "%s\t%s\t%s\t%s\n",
+            "category",
+            "volume",
+            "weight",
+            "mass");
+
+    for (p = 0; p != total_used; ++p)
+    {
+        fprintf(stdout, "%u\t%g\t%g\t%g\n",
+                p,
+                volume[p],
+                weight[p],
+                mass[p]);
+    }
+
+    /* calculate metrics of interest */
+    qsort(volume, total_used, sizeof(double), double_comp);
+    qsort(weight, total_used, sizeof(double), double_comp);
+    qsort(mass, total_used, sizeof(double), double_comp);
+
+    double medians[] = { 
+        volume[total_used / 2],
+        weight[total_used / 2],
+        mass[total_used / 2]
+    };
+    
+    unsigned
+        min_p = (unsigned)floor(npoints * min_quantile),
+        max_p = (unsigned)floor(npoints * max_quantile) - 1,
+        num_used_metrics = max_p - min_p;
+        
+    double means[] = { 0, 0, 0 };
+    for (p = min_p; p != max_p; ++p)
+    {
+        means[0] += volume[p];
+        means[1] += weight[p];
+        means[2] += mass[p];
+    }
+    means[0] /= (double)num_used_metrics;
+    means[1] /= (double)num_used_metrics;
+    means[2] /= (double)num_used_metrics;
+    
+    fprintf(stdout, "%s\t%g\t%g\t%g\n", "mean", means[0], means[1], means[2]);
+    fprintf(stdout, "%s\t%g\t%g\t%g\n", "median", medians[0], medians[1], medians[2]);
+
+    /* clean up */
+    for (d = 0; d != NDIM; ++d)
+        free(mpoints[d]);
+
+    free(mbuf);
+    free(volume);
+    free(weight);
+    free(mass);
+}
+
 
 
 /* compute expected value of nearest neighbor distance, using all
    points inside the 8th square (square with 1/2 of the side length of
    its containing square, centered in the middle) */
-double mean_nn_dist(double *points, int npoints, double half_side, int ndim)
+double mean_nn_dist(struct wpoint *points, int npoints,
+                    int ncomp, 
+                    double (*dist_fcn)(double *p1, double *p2, int ncomp, int *ndim),
+                    double *median_dist, 
+                    double *median_wgt,
+                    int *npoints_used)
 {
-    double **ppoints = (double **)malloc(npoints * sizeof(double *));
+    double *dists = (double *)malloc(npoints * sizeof(double));
+
     /* initialize the points */
-    double **pl, **pr;
-    double *p = points;
-    double **pp = ppoints;
-    while (pp != ppoints + npoints)
-    {
-        *pp++ = p;
-        p += ndim;
-    }
+    struct wpoint *pl, *pr, *pp, *pbeg = points, *pend = pbeg + npoints;
+
+    int d;
+    int ndim;
+
+    /* find the ranges of weight that are safe */
+    qsort(points, npoints, sizeof(struct wpoint), wcmp);
+    double lo_weight = points[(int)floor(npoints * LO_BOUND)].wgt;
+    double hi_weight = points[(int)floor(npoints * HI_BOUND)].wgt;
+    
     cmp_dim = 0;
-    qsort(ppoints, npoints, sizeof(double *), pcmp);
-    double lo_val = -half_side / 2.0, hi_val = half_side / 2.0;
-    double *p_lo = &lo_val, *p_hi = &hi_val;
+    qsort(points, npoints, sizeof(struct wpoint), pcmp);
+    /* struct wpoint **lb = lower_bound(ppoints, npoints, sizeof(double *), &p_lo, pcmp); */
+    /* struct wpoint **ub = upper_bound(ppoints, npoints, sizeof(double *), &p_hi, pcmp); */
 
-    double **lb = lower_bound(ppoints, npoints, sizeof(double *), &p_lo, pcmp);
-    double **ub = upper_bound(ppoints, npoints, sizeof(double *), &p_hi, pcmp);
+    int left_more = 1, right_more = 1, total_count = 0, n = 0;
+    double nn_dist, cur_dist, total_dist = 0, total_prod = 0;
 
-    int left_more = 1, right_more = 1, total_count = 0;
-    double nn_dist, cur_dist, total_dist = 0;
-    for (pp = lb; pp != ub; ++pp)
+    for (pp = pbeg; pp != pend; ++pp)
     {
         pl = pp;
         pr = pp;
-        left_more = pp != ppoints;
-        right_more = pp != ppoints + npoints;
+        int out_of_bounds = 0;
+        for (d = 0; d != ncomp; ++d)
+            if (pp->wgt < lo_weight || pp->wgt > hi_weight)
+            {
+                out_of_bounds = 1;
+                break;
+            }
+
+        /* if (out_of_bounds) continue; */
+
+        left_more = pp != pbeg;
+        right_more = pp != pend;
         nn_dist = DBL_MAX;
 
         while (left_more || right_more)
         {
             if (left_more)
             {
-                if (--pl == ppoints || fabs((*pl)[cmp_dim] - (*pp)[cmp_dim]) > nn_dist)
+                if (--pl == pbeg || fabs(pl->x[cmp_dim] - pp->x[cmp_dim]) > nn_dist)
                     left_more = 0;
 
                 else
-                    nn_dist = ((cur_dist = euclidean_dist(*pl, *pp, ndim)) < nn_dist)
+                    nn_dist = ((cur_dist = dist_fcn(pl->x, pp->x, ncomp, &ndim)) < nn_dist)
                         ? cur_dist : nn_dist;
             }
             if (right_more)
             {
-                if (++pr == ppoints + npoints || fabs((*pr)[cmp_dim] - (*pp)[cmp_dim]) > nn_dist)
+                if (++pr == pend || fabs(pr->x[cmp_dim] - pp->x[cmp_dim]) > nn_dist)
                     right_more = 0;
                 
                 else
-                    nn_dist = ((cur_dist = euclidean_dist(*pr, *pp, ndim)) < nn_dist)
+                    nn_dist = ((cur_dist = dist_fcn(pr->x, pp->x, ncomp, &ndim)) < nn_dist)
                         ? cur_dist : nn_dist;
             }
         }
-        if (pl != ppoints && pr != ppoints + npoints)
+
+        dists[n++] = nn_dist;
+        if (! out_of_bounds && pl != pbeg && pr != pend)
         {
             total_dist += nn_dist;
-            ++total_count;
+            total_prod += pow(nn_dist, ndim) * pp->wgt;
+            total_count++;
         }
-        if ((ub - pp) % 10000 == 0)
-            fprintf(stdout, "%i points left.\n", ub - pp);
+        if ((pend - pp) % 10000 == 0)
+            fprintf(stdout, "%i points left.\n", pend - pp);
     }
-    free(ppoints);
-    return total_dist / (double)total_count;
+    qsort(dists, npoints, sizeof(double), double_comp);
+    *median_dist = dists[npoints / 2];
+
+    qsort(points, npoints, sizeof(struct wpoint), wcmp);
+    *median_wgt = points[npoints / 2].wgt;
+
+    *npoints_used = total_count;
+
+    free(dists);
+    return total_prod / (double)total_count;
 }
 
 
@@ -171,34 +356,82 @@ void die(char *msg, int exitval)
 int main(int argc, char **argv)
 {
     /* */
-    int ndim = atoi(argv[1]);
-    int points_per_unit_vol = atoi(argv[2]);
-    double half_side = atof(argv[3]);
-    int npoints = points_per_unit_vol * gsl_pow_int((int)floor(half_side) * 2.0, ndim);
+    int ncomp = atoi(argv[1]);
+    int ndim_projected = atoi(argv[2]);
+    int npoints = atoi(argv[3]);
+    unsigned G = atoi(argv[4]);
+    char *str_alpha = argv[5];
+
+    global_alpha = (double *)malloc(npoints * 4 * sizeof(double));
+    double alpha[4];
+    sscanf(str_alpha, "%lf,%lf,%lf,%lf", &global_alpha[0], &global_alpha[1], &global_alpha[2], &global_alpha[3]);
+
+    int n_dirichlet_dim = ncomp;
+    double total_volume = 1;
+
+    double points_per_unit_volume = (double)npoints / total_volume;
 
     gsl_rng *rand = gsl_rng_alloc(gsl_rng_taus);
+    struct timespec now;
+    clock_gettime(CLOCK_REALTIME, &now);
+    gsl_rng_set(rand, now.tv_sec);
 
     int c, p, d;
-    double *points = (double *)malloc(npoints * ndim * sizeof(double));
+    struct wpoint *points = (struct wpoint *)malloc(npoints * sizeof(struct wpoint));
+    double *pb, *buf = (double *)malloc(npoints * ndim_projected * sizeof(double));
+
     if (! points)
         die("Couldn't allocate that many points.", 1);
 
-    double *pp, *pe = points + npoints * ndim;
-    /* double *comps = (double *)malloc(ncomps * sizeof(double)); */
-    double mean = 0;
+    struct wpoint *pp, *pe = points + npoints;
+    double mean_vol_times_wgt = 0, median_dist, median_wgt;
+    
+    for (pp = points, pb = buf; pp != pe; ++pp, pb += ndim_projected)
+        pp->x = pb;
 
-    for (pp = points; pp != pe; pp += ndim)
-        for (d = 0; d != ndim; ++d)
-            pp[d] = gsl_ran_flat(rand, -half_side, half_side);
+    /* alternative simulation of points using a Dirichlet */
+    double *comp = (double *)malloc(ncomp * sizeof(double));
+    for (pp = points; pp != pe; ++pp)
+    {
+        gsl_ran_dirichlet(rand, n_dirichlet_dim, global_alpha, comp);
+        pp->wgt = dirichlet_pdf(comp, ncomp);
+        comp_to_simplex(comp, pp->x, ncomp);
+    }
 
-    mean = mean_nn_dist(points, npoints, half_side, ndim);
+    partite_mass(points, npoints, G, LO_BOUND, HI_BOUND);
+    /*
+    int npoints_used;
+    mean_vol_times_wgt = mean_nn_dist(points, npoints, ndim_projected, 
+                                      euclidean_dist, &median_dist, 
+                                      &median_wgt, &npoints_used);
 
-    double density = pow(points_per_unit_vol, 1.0 / (double)ndim);
-    fprintf(stdout, "ndim\tnpoints\tncomps\tmedian\tmean\tmean_times_density\n");
-    fprintf(stdout, "%i\t%i\t%g\t%g\n", ndim, npoints, mean, mean * density);
+    fprintf(stdout, 
+            "%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+            "ncomp",
+            "ndim_projected",
+            "npoints",
+            "npoints_used",
+            "mean",
+            "mean_times_density",
+            "median_dist",
+            "median_wgt",
+            "md_mw_times_density");
+
+    fprintf(stdout, 
+            "%i\t%i\t%i\t%i\t%g\t%g\t%g\t%g\t%g\n",
+            ncomp, ndim_projected, npoints, npoints_used, 
+            mean_vol_times_wgt,
+            mean_vol_times_wgt * points_per_unit_volume, 
+            median_dist,
+            median_wgt,
+            pow(median_dist, ndim_projected) * median_wgt * points_per_unit_volume);
+    */
+    
 
     free(points);
-    /* free(comps); */
+    free(global_alpha);
+    free(buf);
+    free(comp);
     gsl_rng_free(rand);
 
     return 0;
