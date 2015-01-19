84af7e28057916946d775ceacddf5853d646349e
Henry Bigelow <hrbigelow@gmail.com>
2015-01-18 18:42:30 -0800

    adding evaluation counts to metropolis procedure

--- metropolis_sampling.h
+++ metropolis_sampling.h
@@ -25,19 +25,27 @@ struct posterior_settings
     size_t n_dist_quantiles, n_comp_quantiles;
 };
 
+struct eval_counts {
+    unsigned n_dirichlet;
+    unsigned n_yep;
+};
+
 
 size_t tune_proposal(const struct packed_counts *cts,
                      const struct posterior_settings *set,
                      double *proposal_alpha,
                      double *estimated_mean,
-                     double *points_buf);
+                     double *points_buf,
+                     struct eval_counts *eval);
 
-void metropolis_sampling(unsigned short start_point, unsigned short n_points_wanted,
-                         const struct packed_counts *cts,
-                         const double *logu, /* logs of U[0, 1] values */
-                         double *proposal_alpha,
-                         const double *prior_alpha,
-                         size_t nth, /* collect every nth point */
-                         double *sample_points);
+void
+metropolis_sampling(unsigned short start_point, unsigned short n_points_wanted,
+                    const struct packed_counts *cts,
+                    const double *logu, /* logs of U[0, 1] values */
+                    double *proposal_alpha,
+                    const double *prior_alpha,
+                    size_t nth, /* collect every nth point */
+                    double *sample_points,
+                    struct eval_counts *eval);
 
 #endif /* METROPOLIS_SAMPLING_H */
