267703d2a9898f99a39567026ebebcdef1274ba4
Henry Bigelow <hrbigelow@gmail.com>
2015-08-27 11:05:38 -0700

    cleaning up old files etc.

--- indel_diff.h
+++
@@ -1,9 +0,0 @@
-#ifndef _INDEL_DIFF_H
-#define _INDEL_DIFF_H
-
-void next_indel_distance_quantiles_aux(struct dist_worker_input *dw, 
-                                       size_t gs,
-                                       struct managed_buf *buf);
-
-
-#endif /* _INDEL_DIFF_H */
