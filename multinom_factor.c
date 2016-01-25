d614862d3165ad6fc25fd13d706b4643e483df44
hrbigelow <hrbigelow@gmail.com>
2016-01-25 15:58:44 -0800

    progress on multinom_factor

--- multinom_factor.c
+++ multinom_factor.c
@@ -21,51 +21,146 @@ struct factor_exp {
 /* hold the factorizations for each number */
 static struct factor_exp *g_factors[MAX_FACTOR];
 
+/* factorial factorizations */
+static struct factor_exp *g_factorial_pff[MAX_FACTOR];
+
 /* compute factorizations and primes */
 void
 factorize()
 {
     unsigned expon[MAX_FACTOR];
-    unsigned n, p, i, j, prod, carry;
+    unsigned n, p, s, e, i, j, prod, carry;
 
     /* initialize */
     memset(expon, 0, sizeof(expon));
+    for (i = 0; i != MAX_FACTOR; ++i) g_factors[i] = NULL;
+
     g_n_primes = 0;
-    n = 1;
-    
-    while (n < MAX_FACTOR) {
-        
+    n = 2;
+    prod = 2;
+
+    while (n != MAX_FACTOR) {
+        primes[g_n_primes++] = n;
+
         /* this loop generates all new factorizations that involve
            p */
         while (0) {
             if (prod < MAX_FACTOR) {
                 /* record factorization */
-                g_factors[n] = malloc(sizeof(struct factor_exp)
-                                          + sizeof(int) * 1);
-                for (i = 0; expon[i] == 0; i++) ;
-                g_factors[n]->s = i;
-                for (j = 0; expon[i] != 0; ++i, ++j)
-                    g_factors[n]->factors[j] = expon[i];
-                g_factors[n]->e = i;
+                for (s = 0; expon[s] == 0; s++) ;
+                for (e = s + 1; expon[e] != 0; ++e) ;
+                struct factor_exp *spp = malloc(sizeof(struct factor_exp) + sizeof(int) * (e - s));
+
+                spp->s = s;
+                spp->e = e;
+                for (i = s, j = 0; i != e; ++i, ++j)
+                    spp->factors[j] = expon[i];
+                g_factors[prod] = spp;
+
+                ++expon[0];
+                prod *= primes[0];
             }
             else {
                 /* overflow. */
-                for (i = 0; i != carry; ++i) expon[i] = 0;
-                expon[carry]++;
-
-                /* re-compute prod */
-                for (i = carry, prod = 1; i != n_prime; ++i)
-                    prod *= exp(primes[i], expon[i]);
-
-                if (prod >= MAX_FACTOR) {
-                    ++carry;
-                    if (carry > n_primes)
-                        break;
+                i = 0;
+                while (i != g_n_primes && prod >= MAX_FACTOR) {
+                    prod /= exp(primes[i], expon[i]);
+                    expon[i++] = 0;
+                    ++expon[i];
+                    prod *= primes[i];
                 }
-                else carry = 1;
+                if (i == g_n_primes) break;
             }
-            expon[carry - 1]++;
-            prod *= primes[carry-1];
         }
+        /* advance n to next prime < MAX_FACTOR if it exists */
+        while (g_factors[n++] && n != MAX_FACTOR)
+            ;
     }
+}
+
+
+void
+factorial_factorize()
+{
+    unsigned n, s = MAX_FACTOR, e = 0;
+    int rsum[MAX_FACTOR];
+    memset(rsum, 0, sizeof(rsum));
+
+    for (n = 2; n != MAX_FACTOR; ++n) {
+        s = MIN(s, g_factors[n]->s);
+        e = MAX(e, g_factors[n]->e);
+        for (i = s; i != e; ++i)
+            rsum[i] += g_factors[n]->factors[i];
+        struct factor_exp *fe = malloc(sizeof(struct factor_exp) + sizeof(int) * (e - s));
+        fe->s = s;
+        fe->e = e;
+        memcpy(fe->factors, rsum + s, (e - s) * sizeof(rsum[0]));
+        g_factorial_pff[n] = fe;
+    }
+}
+
+
+/* compute (a+b+c)!/a!b!c!, returning a representation in pf form.
+   assume a >= b >= c. */
+struct factor_exp *
+d_choose_abc(unsigned a, unsigned b, unsigned c)
+{
+    int rsum[MAX_FACTOR];
+    unsigned d = a + b + c;
+    assert(d < MAX_FACTOR);
+    struct factor_exp *pf = g_factorial_pff[d];
+    unsigned s = pf->s, e = pf->e;
+    memcpy(rsum + s, pf->factors, (e - s) * sizeof(int));
+
+    unsigned i, j;
+    pf = g_factorial_pff[a];
+    for (i = pf->s, j = 0; i != pf->e; ++i, ++j) rsum[i] -= pf->factors[j];
+
+    pf = g_factorial_pff[b];
+    for (i = pf->s, j = 0; i != pf->e; ++i, ++j) rsum[i] -= pf->factors[j];
+
+    pf = g_factorial_pff[c];
+    for (i = pf->s, j = 0; i != pf->e; ++i, ++j) rsum[i] -= pf->factors[j];
+
+    while (rsum[s] == 0) ++s;
+    while (rsum[e-1] == 0) --e;
+
+    pf = malloc(sizeof(struct factor_exp) + (e - s) * sizeof(int));
+    pf->s = s;
+    pf->e = e;
+    i = s;
+    j = 0;
+    while (i != e) pf->factors[j++] = rsum[i++];
+
+    return pf;
+}
+
+
+
+
+/* reify a pff.  */
+double
+reify_pff(int *exp, unsigned s, unsigned e)
+{
+    double val = 1.0;
+    unsigned i;
+    for (i = 0; i != pff->e - pff->s; ++i)
+        val *= exp();
+}
+
+
+/* compute the full adjustment coefficient */
+double
+adjust_coeff(struct factor_exp *n1,
+             struct factor_exp *n2,
+             struct factor_exp *d)
+{
+    int rsum[MAX_FACTOR];
+    memset(rsum, 0, sizeof(rsum));
+    memcpy(rsum + n1->s, n1->factors, (n1->e - n1->s) * sizeof(int));
+    unsigned i, j;
+    for (i = n2->s, j = 0; i != n2->e; ++i, ++j) rsum[i] += n2->factors[j];
+    for (i = d->s, j = 0; i != d->e; ++i, ++j) rsum[i] -= d->factors[j];
+
+    return reify_pff(rsum + s, s, e);
 }
